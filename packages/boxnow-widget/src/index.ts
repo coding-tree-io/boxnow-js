d171b1ca76a198ccd7abfe621597be2aacd2a63a
zantoichi <s.d.vallianos@gmail.com>
2026-05-12 17:13:56 +0300

    feat(02-05): expose curated package contracts

---
+++ packages/boxnow-widget/src/index.ts
@@ -0,0 +1,16 @@
+export type {
+  BoxNowCountryCode,
+  BoxNowValidationIssue,
+  BoxNowValidationResult,
+  Locker,
+  LockerSnapshot,
+  RawWidgetLockerSelection,
+} from '@coding-tree-io/boxnow-core';
+export {
+  BoxNowCountryCodeSchema,
+  LockerSchema,
+  LockerSnapshotSchema,
+  normalizeLockerSelection,
+  RawWidgetLockerSelectionSchema,
+  validateWithSchema,
+} from '@coding-tree-io/boxnow-core';
